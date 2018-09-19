e2b8a7b3cd373ab9c06e4936aec4e91da954ae02
adamlstephenson <astephensonx@gmail.com>
2018-09-18 20:25:38 -0500

    basic components folders created and initial scaffolding

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Home from './components//Home/Home.js'
+import Nav from './components/Nav/Nav.js'
+//import Footer from './Footer/Footer.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Nav/>
+        <Home />
+        {/* <Footer /> */}
       </div>
     );
   }
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+//import '/Home.css';
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>Hello World</p>
+            </div>
+        )
+    }
+}
+
+export default Home;
