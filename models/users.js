32a7897ec981e336dc056ab6575e765d82a17ab5
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-09-26 19:30:20 -0500

    added phone number to user model

--- models/users.js
+++ models/users.js
@@ -11,6 +11,10 @@ const userSchema = new Schema ({
         type: String,
         required: [true]
     },
+
+    phoneNumber: {
+        type: Number
+    }
 },
 
 {timestamps: true});
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const routes = require("./routes");
+
 
 
 const PORT = process.env.PORT || 3001;
@@ -16,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   } 
 
-//   app.use(routes);
+  app.use(routes);
 
   app.listen(PORT, function() {
     console.log(`API server listening on PORT ${PORT}!`);
