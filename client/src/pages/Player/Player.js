a3b7ac8a17dbd463053a68982f2a78b02a0a1340
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-10-24 20:27:26 -0500

    changed player.js in pages format

--- client/src/pages/Player/Player.js
+++ client/src/pages/Player/Player.js
@@ -20,11 +20,21 @@ class Player extends Component {
                 </div>
                 <div className="form-group">
                     <label htmlFor="lastName">Last Name</label>
-                    <input type="text" className="form-control form-control-lg" id="lastName" aria-describedby="lastNameInput" placeholder=""/>
+                    <input 
+                        type="text" 
+                        className="form-control form-control-lg" 
+                        id="lastName" 
+                        aria-describedby="lastNameInput" 
+                        placeholder=""/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="phoneNumber">Phone Number</label>
-                    <input type="text" className="form-control form-control-lg" id="phoneNumber" aria-describedby="phoneNumberHelp" placeholder=""/>
+                    <input 
+                        type="text" 
+                        className="form-control form-control-lg" 
+                        id="phoneNumber" 
+                        aria-describedby="phoneNumberHelp" 
+                        placeholder=""/>
                     <small id="phoneNumberHelp" class="form-text text-muted">For getting the squad together!</small>
                 </div>
                 <button type="submit" class="btn btn-primary">Add Player</button>
